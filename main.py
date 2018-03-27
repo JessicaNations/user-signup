be7ee76335af72950de879e2fb57d09e68de8ef7
Jessica Nations <jessica.nations@gmail.com>
2018-03-27 07:06:58 -0500

    uptodate

--- main.py
+++ main.py
@@ -1,69 +1,24 @@
-from flask import Flask, request, redirect
-import cgi
+from flask import Flask, request
+import os
+import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__),'templates')
+
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
 app = Flask(__name__)
-
 app.config['DEBUG'] = True
-
-page_header = """
-<!DOCTYPE html>
-<html>
-    <head>
-     <style>
-      .error {
-          color:red;
-      }
-     </style>
-    </head>
-    <body>
-    <h1>Signup</h1>
-"""
-page_footer = """
-    </body>
-</html>
-"""
-form = """
-      <form method="port">
-        <table>
-          <tr>
-            <td><label for="username">Username</label>
-</td>
-            <td>
-              <input name="username" type="text" value="">
-              <span class="eror"></span>
-            </td>
-          </tr>
-          <tr>
-            <td><label for="password">Password</label></td>
-            <td>
-              <input name="password" type="password">
-              <span class="error"></span>
-            </td>
-          </tr>
-          <tr>
-            <td><labe for="verify">Verify Password</label>
-</td>
-            <td>
-              <input name="verify" type="password">
-              <span class="error"></span>
-            </td>
-          </tr>
-          <tr>
-            <td><label for="email">Email (optional)</label>
-</td>
-            <td>
-              <input name="email" value="">
-              <span class="error"></span>
-            </td>
-          </tr>
-        </table>
-        <input type="submit">
-      </form>
-"""
 
 @app.route("/")
 def index():
-    content = page_header + "<p>" + form + "</p>" + page_footer 
-    return content
-    
-app.run() 
+	template = jinja_env.get_template('home_page.html')
+	return template.render()
+
+@app.route("/hello", methods=['POST'])
+def hello():
+	form_username = request.form['username']
+	template = jinja_env.get_template('welcome_page.html')
+	return template.render(username=form_username)
+
+
+app.run()
